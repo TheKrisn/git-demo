301edc02ad376e90282ca49491bb31ff337785cf
TheKrisn <odinstarkop@gmail.com>
2024-04-05 13:42:35 +0530

    feat: division

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
-import { add, sub, mul } from "./math";
+import { add, sub, mul, div } from "./math";
 
 const sum = add(2, 2);
 const product = mul(2, 2);
-
+const division = div(2, 2);
 const minus = sub(2, 2);
 
 console.log("Sum", sum);
 console.log("Minus", minus);
 console.log("Product", product);
+console.log("Division", division);
--- math.ts
+++ math.ts
@@ -9,3 +9,6 @@ export function mul(a: number, b: number): number {
 export function sub(a: number, b: number): number {
   return a - b;
 }
+export function div(a: number, b: number): number {
+  return a / b;
+}
